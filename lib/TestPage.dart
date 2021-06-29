641893a954d056eb876d6e32a9d7e4767982224d
Eashaankar Sahai <eashaankarsahai@hotmail.com>
2021-06-29 19:14:45 +0530

    linked to Realtime Database - Firebase

---
+++ lib/TestPage.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+
+
+class TestPage extends StatefulWidget {
+
+  TestPage({required this.app});
+  final FirebaseApp app;
+
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+
+  final referenceDatabase = FirebaseDatabase.instance;
+  final taskfirename = 'TaskTitle';
+  final customController = TextEditingController();
+
+  //final List<String> tasknames = <String>[];
+
+  /*void addItemToList(){
+    setState(() {
+      tasknames.insert(0,customController.text);
+    });
+  }*/
+
+  Future _asynccreateAlertDialog(BuildContext context) async{
+
+    final ref = referenceDatabase.reference();
+
+
+    var alert = AlertDialog(
+      title: Text("Enter Task"),
+      content: TextField(
+        controller: customController,
+      ),
+      actions: <Widget>[
+        MaterialButton(
+          elevation: 4.0,
+          child: Text('Submit'),
+          onPressed: (){
+            //addItemToList();
+            ref
+                .child('TaskNameF')
+                .push()
+                .child(taskfirename)
+                .set(customController.text)
+                .asStream();
+            customController.clear();
+            Navigator.of(context).pop(customController.text.toString());
+            final snackBar = SnackBar(
+              content: Text('Added' + customController.text.toString()),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  // Some code to undo the change.
+                },
+              ),
+            );
+
+            // Find the ScaffoldMessenger in the widget tree
+            // and use it to show a SnackBar.
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+        )
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
+  }
+
+  late DatabaseReference _taskName;
+
+  @override
+  void initState(){
+    final FirebaseDatabase database = FirebaseDatabase(app: widget.app);
+    _taskName = database.reference().child('TaskNameF');
+
+    super.initState();
+  }
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('To-Do list')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    Flexible(
+                        child: new FirebaseAnimatedList(
+                          shrinkWrap: true,
+                            query: _taskName,
+                            itemBuilder: (BuildContext context,
+                        DataSnapshot snapshot,
+                        Animation<double> animation,
+                        int index){
+                      return new ListTile(
+                          trailing: IconButton(icon: Icon(Icons.delete),
+                            onPressed: () => _taskName.child(snapshot.key).remove(),),
+                          title: new Text(snapshot.value['TaskTitle'] ?? ''),
+                      );
+                    })
+                      /*child: ListView.builder(
+                          padding: const EdgeInsets.all(8),
+                          itemCount: tasknames.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Dismissible(
+                              child: ListTile(
+                                title: Text('${tasknames[index]}', style: TextStyle(fontSize: 18),),
+                              ),
+                              background: Container(
+                                color: Colors.green,
+                                child: Padding(
+                                  padding: EdgeInsets.only(left: 0.1),
+                                  child: Icon(Icons.check,
+                                  color: Colors.white,),
+                                ),
+                              ),
+                              key: ValueKey<int>(index),
+                              onDismissed: (DismissDirection direction){
+                                setState(() {
+                                  tasknames.removeAt(index);
+                                });
+                              },
+
+                            );
+                          },
+                        )*/),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          _asynccreateAlertDialog(context);
+          print('Pressed');
+        },
+        label: const Text('Add Task'),
+        icon: const Icon(Icons.add),
+        backgroundColor: Colors.black,
+        hoverColor: Colors.black54,
+      ),
+    );
+  }
+  /*@override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<DatabaseReference>('_taskName', _taskName));
+  }*/
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,22 @@
+// @dart=2.9
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list/TestPage.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black),
-      home: TestPage(),
-    );
-  }
+//void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp();
+  runApp(MaterialApp(
+    theme: ThemeData(primaryColor: Colors.black),
+    home: TestPage(app: app),
+  ));
 }
 
-TextEditingController customController = TextEditingController();
-
-class TestPage extends StatefulWidget {
 
 
-  @override
-  _TestPageState createState() => _TestPageState();
-}
-
-class _TestPageState extends State<TestPage> {
-
-  final referenceDatabase = FirebaseDatabase.instance;
-  final taskfirename = 'TaskTitle';
-
-  final List<String> tasknames = <String>[];
-
-  void addItemToList(){
-    setState(() {
-      tasknames.insert(0,customController.text);
-    });
-  }
-
-  Future _asynccreateAlertDialog(BuildContext context) async{
-
-
-    var alert = AlertDialog(
-      title: Text("Enter Task"),
-      content: TextField(
-        controller: customController,
-      ),
-      actions: <Widget>[
-        MaterialButton(
-          elevation: 4.0,
-          child: Text('Submit'),
-          onPressed: (){
-            addItemToList();
-            Navigator.of(context).pop(customController.text.toString());
-            final snackBar = SnackBar(
-              content: Text('Added ' + customController.text),
-              action: SnackBarAction(
-                label: 'Undo',
-                onPressed: () {
-                  // Some code to undo the change.
-                },
-              ),
-            );
-
-            // Find the ScaffoldMessenger in the widget tree
-            // and use it to show a SnackBar.
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          },
-        )
-      ],
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    final ref = referenceDatabase.reference();
-    return Scaffold(
-      appBar: AppBar(title: Text('To-Do list')),
-      body: Column(
-        children: [
-          Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: tasknames.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Dismissible(
-                    child: ListTile(
-                      title: Text('${tasknames[index]}', style: TextStyle(fontSize: 18),),
-                    ),
-                    background: Container(
-                      color: Colors.green,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 0.1),
-                        child: Icon(Icons.check,
-                        color: Colors.white,),
-                      ),
-                    ),
-                    key: ValueKey<int>(index),
-                    onDismissed: (DismissDirection direction){
-                      setState(() {
-                        tasknames.removeAt(index);
-                      });
-                    },
-
-                  );
-                },
-              )),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          _asynccreateAlertDialog(context);
-          print('Pressed');
-        },
-        label: const Text('Add Task'),
-        icon: const Icon(Icons.add),
-        backgroundColor: Colors.black,
-        hoverColor: Colors.black54,
-      ),
-    );
-  }
-}
 
 
 
